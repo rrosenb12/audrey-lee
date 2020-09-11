1c7b5d81e2bd21b4a8b566da8de3be0819516405
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-11 17:45:38 -0400

    navbar and footer styling

---
+++ src/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import facebookglpyh from './glyphs/facebook.svg'
+import twitterglyph from './glyphs/twitter.svg'
+import instaglyph from './glyphs/instagram.svg'
+import linkedinglyph from './glyphs/linkedin.svg'
+import {NavLink} from 'react-router-dom'
+
+export default function NavBar(){
+    return(
+        <>
+            <div class="navbar is-fixed-top">
+                <p className="navbaritem">Publications</p>
+                <p className="navbaritem">Probably, Angels</p>
+                <h1 className="navbartitle">Audrey Lee</h1>
+                <p className="navbaritem">Awards & Recognitions</p>
+                <p className="navbaritem">Media</p>
+            </div>
+            
+            <div className="footer">
+                <img className="contactglyph" src={facebookglpyh} height="30"/>
+                <img className="contactglyph" src={twitterglyph} height="30"/>
+                <img className="contactglyph" src={instaglyph} height="30"/>
+                <img className="contactglyph" src={linkedinglyph} height="30"/>
+            </div>
+        </>
+    )
+}
