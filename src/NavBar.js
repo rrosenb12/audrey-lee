0c94a0f10b96055052986c417d55bdaa9a2161be
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-15 16:13:33 -0400

    rotating image for smaller screens

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,29 +2,45 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 import ddbutton from './glyphs/ddbutton.png'
 
-export default function NavBar(){
-    return(
-        <>
-            <div className="navbarrow">
-                <div className="navbar">
-                    <p className="navbaritem">Publications</p>
-                    <p className="navbaritem">Probably, Angels</p>
-                    <NavLink to="/">
-                        <h1 className="navbartitle">Audrey Lee</h1>
-                    </NavLink>
-                    <p className="navbaritem">Awards & Recognitions</p>
-                    <p className="navbaritem">Media</p>
-                </div>
-            </div>
+export default class NavBar extends React.Component{
 
-            <div className="smallnavbarrow">
-            <img className="ddbutton" src={ddbutton} alt='ddbutton' height="50"/>
-                <div className="titlewrapper">
-                    <NavLink to="/">
-                        <h1 className="navbartitle">Audrey Lee</h1>
-                    </NavLink>
+    state = {
+        rotate: false
+    }
+
+    clickHandler = (e) => {
+        console.log(e.target.className)
+        this.setState(previousState => {
+            return {
+                rotate: !previousState.rotate
+            }
+        }, () => console.log(this.state.rotate))
+    }
+
+    render(){
+        return(
+            <>
+                <div className="navbarrow">
+                    <div className="navbar">
+                        <p className="navbaritem">Publications</p>
+                        <p className="navbaritem">Probably, Angels</p>
+                        <NavLink to="/">
+                            <h1 className="navbartitle">Audrey Lee</h1>
+                        </NavLink>
+                        <p className="navbaritem">Awards & Recognitions</p>
+                        <p className="navbaritem">Media</p>
+                    </div>
                 </div>
-            </div>
-        </>
-    )
+
+                <div className="smallnavbarrow">
+                    <img src={ddbutton} alt='ddbutton' onClick={this.clickHandler} id="ddbutton" className={this.state.rotate ? "rotate" : ""} height="50"/>
+                    <div className="titlewrapper">
+                        <NavLink to="/">
+                            <h1 className="navbartitle">Audrey Lee</h1>
+                        </NavLink>
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
