169b3d3e4228d5a9fc048ece41ba8cf03ef090c6
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-22 16:37:12 -0400

    masonry added to pubcards

--- src/components/PubContent.js
+++ src/components/PubContent.js
@@ -2,15 +2,20 @@ import React from "react";
 
 export default function PubContent(props) {
   return (
-    <div>
+    <div className="pub-cards-wrapper">
       {props.publications.map((publication) => {
         return (
           <div className="pub-card" type={publication.type}>
-            <a href={publication.url} target="_blank" rel="noopener noreferrer">
+            <a
+              className="pub-title"
+              href={publication.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               {publication.title}
             </a>
             {publication.publication.map((pub) => (
-              <p>{pub}</p>
+              <p className="pub-pub">{pub}</p>
             ))}
           </div>
         );
