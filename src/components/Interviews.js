27610c71ef54138dd171ee358e3af514fe7b67da
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-09-29 19:11:26 -0400

    how to embed videos

---
+++ src/components/Interviews.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Interviews(){
+    return(
+        <h1>interviews</h1>
+    )
+}
---
+++ src/components/Videos.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Videos() {
+  return (
+    <>
+      <div className="videos-wrapper">
+        <button className="back-button">Back</button>
+        <h1>videos</h1>
+        <iframe title="Audrey reading `Indiana, Hotter`" src="https://www.facebook.com/watch/?v=1034940300211359"></iframe>
+      </div>
+    </>
+  );
+}
