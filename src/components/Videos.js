298f39c1afef7aa34e30e355e42edc763f30311a
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-10-01 19:46:35 -0400

    facebook video loading

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -9,20 +9,24 @@ export default function Videos() {
           <button className="back-button">Back</button>
         </NavLink>
         <h1>videos</h1>
-        <div
-          class="fb-video"
-          data-href="https://www.facebook.com/PhiladelphiaAlumniWritersHouse/videos/1034940300211359"
-          data-allowfullscreen="true"
-          data-width="300"
-        ></div>
+        <iframe
+          src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FPhiladelphiaAlumniWritersHouse%2Fvideos%2F1034940300211359%2F&show_text=0&width=560"
+          width="560"
+          height="315"
+          scrolling="no"
+          frameborder="0"
+          allowTransparency="true"
+          allowFullScreen="true"
+          title="Audrey reading `Indiana, Hotter` for the Franklin & Marshall College WRiters House Social Distancing Salon, March 2020"
+        ></iframe>
         <iframe
           width="560"
           height="315"
           src="https://www.youtube.com/embed/xiFEec0Q0xE"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-          title="Audrey reading at the Episcopal AcademyW"
+          allowFullScreen="true"
+          title="Audrey reading at the Episcopal Academy"
         ></iframe>
       </div>
     </>
