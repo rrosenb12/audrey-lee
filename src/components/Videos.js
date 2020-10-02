baa7cec7cc4ba7e1c4fc385ad7b930e56b1e0720
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-10-02 19:48:43 -0400

    video page non-mobile styling done

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -6,29 +6,37 @@ export default function Videos() {
   return (
     <>
       <div className="videos-wrapper">
-        <NavLink to="/media">
-          <button className="back-button">Back</button>
-        </NavLink>
-        <h1>videos</h1>
-        <iframe
-          src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FPhiladelphiaAlumniWritersHouse%2Fvideos%2F1034940300211359%2F&show_text=0&width=560"
-          width="560"
-          height="315"
-          scrolling="no"
-          frameborder="0"
-          allowTransparency="true"
-          allowFullScreen="true"
-          title="Audrey reading `Indiana, Hotter` for the Franklin & Marshall College WRiters House Social Distancing Salon, March 2020"
-        ></iframe>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/xiFEec0Q0xE"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen="true"
-          title="Audrey reading at the Episcopal Academy"
-        ></iframe>
+        <div className="videos-top">
+          <div className="button-wrapper">
+            <NavLink to="/media">
+              <button className="back-button">Back</button>
+            </NavLink>
+          </div>
+          <h1 className="videos-title">Videos</h1>
+        </div>
+        <div className="content-wrapper">
+          <iframe
+            className="video"
+            src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FPhiladelphiaAlumniWritersHouse%2Fvideos%2F1034940300211359%2F&show_text=0&width=560"
+            width="560"
+            height="315"
+            scrolling="no"
+            frameborder="0"
+            allowTransparency="true"
+            allowFullScreen="true"
+            title="Audrey reading `Indiana, Hotter` for the Franklin & Marshall College WRiters House Social Distancing Salon, March 2020"
+          ></iframe>
+          <iframe
+            className="video"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/xiFEec0Q0xE"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen="true"
+            title="Audrey reading at the Episcopal Academy"
+          ></iframe>
+        </div>
       </div>
       <Footer />
     </>
